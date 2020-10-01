9f3983ac5b6fe91564997e3e9421672a70c1cfbf
raph santana <354184@colum.edu>
2020-10-01 18:07:30 -0500

    Weather_api_works

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import * as OpenWeather from "openweather-apis";
-
+import Conditions from "./conditions/conditions";
+import conditions from "./conditions/conditions";
 
 /**
  * This module will be connected the open weather API and will fetch for weather data based on the location provided by the user
@@ -9,47 +10,45 @@ import * as OpenWeather from "openweather-apis";
  */
 
 const Weather = () => {
-
   const [responseObj, setResponseObj] = useState({});
+  let [unit, setUnit] = useState('imperial')
 
   function getWeather() {
-
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "8663513905msh5590d4c73aa2beap1ac33cjsn8972ecb9adec"
+    fetch(
+      `https://community-open-weather-map.p.rapidapi.com/weather?q=Chicago&units=${unit}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key":
+            "8663513905msh5590d4c73aa2beap1ac33cjsn8972ecb9adec",
+        },
       }
-    })
-    .then(response => response.json())
-       .then(response => {
-           setResponseObj(response)
-       })
-  
-     }
-  
-  
-  return (
-  
-    <div>
-    <h2>Today weather in Chicago</h2>
-    <div>
-        {JSON.stringify(responseObj)}
-    </div>
-    <button onClick={getWeather}>Get Weather</button>
-</div>
-  
-     )
-  
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setResponseObj(response);
+      });
   }
-  
-  
-  export default Weather;
+
+  return (
+    <div> 
+      
+                
+      <button onClick={getWeather}>Get Weather</button>
+      
+      <Conditions responseObj={responseObj} />
+
+    </div>
+  );
+};
+
+export default Weather;
 
 // function Weather() {
 
 //     const GetWeather
-  
+
 //   return (
 //     <div>
 //            <h2>Find Current Weather Conditions</h2>
@@ -60,7 +59,6 @@ const Weather = () => {
 //        </div>
 //   );
 
-  
 // }
 
 // export default Weather;
