ac45f0cac261b4e7a144cbc650166360993a2f1b
Rafael <354184@colum.edu>
2020-12-13 14:15:07 -0600

    logo_

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css"
+import logo from "../images/hub-logo.png"
 
 function Nav(props) {
   return (
     <nav className="nav">
+      <img src={logo} alt=""/>
       <Link className="nav__link" to="/account">
         {props.user ? "Account" : "Login"}
       </Link>{" "}
