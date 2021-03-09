d44c0e39460b62b15fee9db8b41bccb22bc019cb
Raph Santana <rsantana@colum.edu>
2021-03-08 20:19:10 -0600

    Simulating conflict for homework.

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { withRouter } from 'react-router'
-import Events from '../components/events'
-import Header from '../components/header'
-import SpotifyPlayer from '../components/spotify-player'
-import Weather from '../components/weather'
-import "./main-page.css"
+import React from "react";
+import { withRouter } from "react-router";
+import Events from "../components/events";
+import Header from "../components/header";
+import SpotifyPlayer from "../components/spotify-player";
+import Weather from "../components/weather";
+import "./main-page.css";
 
 function MainPage(props) {
-    
-    return (
-        <div className="grid-container">
-            <Header className="grid-elements" {...props}/>
-            <Weather className="grid-elements" {...props}/>
-            <Events className="grid-elements" {...props}/>
-            <SpotifyPlayer className="spotify"/>
-
-        </div>
-    )
+  return (
+    <div className="grid-container">
+      <Header className="grid-elements" {...props} />
+      <Weather className="grid-elements" {...props} />
+      <Events className="grid-elements" {...props} />
+      <SpotifyPlayer className="spotify" />
+      <div>I'm also trying to cause conflict</div>
+    </div>
+  );
 }
 
-export default withRouter(MainPage)
+export default withRouter(MainPage);
